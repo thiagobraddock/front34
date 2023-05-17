2db9971692601b71840f943c139958922cbbb549
Moises Santana <moisaant@gmail.com>
2023-05-17 17:55:33 -0300

    :sparkles: section 1

--- src/components/Sections/Projects/index.tsx
+++ src/components/Sections/Projects/index.tsx
@@ -1,4 +1,4 @@
-import phone from '../../../assets/images/hero/phone.png';
+import iChoveu from '../../../assets/images/projects/iChoveu.svg';
 
 function Projects() {
   return (
@@ -9,16 +9,18 @@ function Projects() {
             <div className="col-lg-6 col-md-12 col-12">
 
               <div className="info-text wow fadeInLeft" data-wow-delay=".3s" style={ { visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInLeft' } }>
-                <div className="main-icon">
+                {/* <div className="main-icon">
                   <i className="lni lni-inbox" />
-                </div>
-                <h2>1,25,000 Customers Using The Application!</h2>
+                </div> */}
+                <h2>iChoveu</h2>
                 <p>
-                  Collaborate over projects with your team and clients optimised for mobile and tablet
-                  don&#39;t
-                  let slow page speeds drive our innovative platform empowers anyone to convert clicks
-                  ou&#39;ll
-                  publish your first landing page in minutes.
+                  Projeto que aborda funções assíncronas e requisições a API.
+                  {' '}
+                  Você vai construir uma aplicação que consulta a API
+                  {' '}
+                  <strong>weatherapi</strong>
+                  {' '}
+                  e exibe a previsão do tempo para a cidade pesquisada.
                 </p>
                 <div className="button">
                   <a href="" className="btn">Get Started</a>
@@ -28,7 +30,7 @@ function Projects() {
             </div>
             <div className="col-lg-6 col-md-12 col-12">
               <div className="info-image wow fadeInRight" data-wow-delay=".5s" style={ { visibility: 'visible', animationDelay: '0.5s', animationName: 'fadeInRight' } }>
-                <img className="ss1" alt="#" data-pagespeed-url-hash="2327635917" src={ phone } />
+                <img className="ss1" alt="#" data-pagespeed-url-hash="2327635917" src={ iChoveu } />
               </div>
             </div>
           </div>
@@ -37,7 +39,7 @@ function Projects() {
           <div className="row align-items-center">
             <div className="col-lg-6 col-md-12 col-12">
               <div className="info-image wow fadeInLeft" data-wow-delay=".3s" style={ { visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInLeft' } }>
-                <img className="ss1" alt="#" data-pagespeed-url-hash="2622135838" src={ phone } />
+                {/* <img className="ss1" alt="#" data-pagespeed-url-hash="2622135838" src={ phone } /> */}
               </div>
             </div>
             <div className="col-lg-6 col-md-12 col-12">
