3feb520918a8521acdb1e8bc70a649ee7f9c28fc
Moises Santana <moisaant@gmail.com>
2023-05-18 16:34:36 -0300

    feat(Projects): :sparkles: Section 4
    React Testing Library Project card from section 4

--- src/components/Sections/Projects/index.tsx
+++ src/components/Sections/Projects/index.tsx
@@ -1,10 +1,12 @@
 import iChoveu from '../../../assets/images/projects/iChoveu.svg';
+import rtl from '../../../assets/images/projects/rtl.png';
+import passwordManager from '../../../assets/images/projects/password-manager.svg';
 
 function Projects() {
   return (
     <section id="overview" className="app-info section">
       <div className="container">
-        <div className="info-one">
+        <div className="info-one style2">
           <div className="row align-items-center">
             <div className="col-lg-6 col-md-12 col-12">
 
@@ -16,14 +18,16 @@ function Projects() {
                 <p>
                   Projeto que aborda funções assíncronas e requisições a API.
                   {' '}
-                  Você vai construir uma aplicação que consulta a API
+                  <br />
+                  {' '}
+                  Você irá desenvolver uma aplicação que consulta a API
                   {' '}
                   <strong>weatherapi</strong>
                   {' '}
                   e exibe a previsão do tempo para a cidade pesquisada.
                 </p>
                 <div className="button">
-                  <a href="" className="btn">Get Started</a>
+                  <a href="" className="btn">Iniciar</a>
                 </div>
               </div>
 
@@ -39,7 +43,7 @@ function Projects() {
           <div className="row align-items-center">
             <div className="col-lg-6 col-md-12 col-12">
               <div className="info-image wow fadeInLeft" data-wow-delay=".3s" style={ { visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInLeft' } }>
-                {/* <img className="ss1" alt="#" data-pagespeed-url-hash="2622135838" src={ phone } /> */}
+                <img className="ss1" alt="#" data-pagespeed-url-hash="2622135838" src={ passwordManager } />
               </div>
             </div>
             <div className="col-lg-6 col-md-12 col-12">
@@ -49,19 +53,88 @@ function Projects() {
                 data-wow-delay=".5s"
                 style={ { visibility: 'visible', animationDelay: '0.5s', animationName: 'fadeInRight' } }
               >
-                <div className="main-icon">
-                  <i className="lni lni-layout" />
-                </div>
-                <h2>Seamless Loyalty</h2>
+                <h2>Password Manager</h2>
                 <p>
-                  Collaborate over projects with your team and clients optimised for mobile and tablet
-                  don&#39;t
-                  let slow page speeds drive our innovative platform empowers anyone to convert clicks
-                  ou&#39;ll
-                  publish your first landing page in minutes.
+                  Projeto em
+                  {' '}
+                  <strong>React</strong>
+                  {' '}
+                  com
+                  {' '}
+                  <strong>TypeScript</strong>
+                  {' '}
+                  desenvolvendo habilidades de controle de estado, eventos em react passagem de props, renderização condicional elevação de estado listagem de componentes de maneira dinâmica.
+                  {' '}
+                  <br />
+                  Você irá desenvolver uma aplicação que cria e gerencia senhas utilizadas em sites e aplicativos.
                 </p>
                 <div className="button">
-                  <a href="" className="btn">Get Started</a>
+                  <a href="" className="btn">Iniciar</a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="info-one style2">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-12 col-12">
+              <div className="info-text wow fadeInLeft" data-wow-delay=".3s" style={ { visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInLeft' } }>
+                <h2>Trybe Tunes</h2>
+                <p>
+                  Projeto que aborda efeitos colaterais do
+                  {' '}
+                  <strong>React</strong>
+                  , rotas e requisições.
+                  {' '}
+                  <br />
+                  {' '}
+                  Você irá desenvolver uma aplicação semelhante ao iTunes, onde terá uma página de login. Após fazer o login você poderá pesquisar por artistas e ver os álbuns e músicas relacionadas a ele. Nele é possível também favoritar músicas e vê-las em uma página separada além de alterar suas informações de usuário.
+                </p>
+                <div className="button">
+                  <a href="" className="btn">Iniciar</a>
+                </div>
+              </div>
+
+            </div>
+            <div className="col-lg-6 col-md-12 col-12">
+              <div className="info-image wow fadeInRight" data-wow-delay=".5s" style={ { visibility: 'visible', animationDelay: '0.5s', animationName: 'fadeInRight' } }>
+                <img className="ss1" alt="#" data-pagespeed-url-hash="2327635917" src={ iChoveu } />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="info-one style2">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-12 col-12">
+              <div className="info-image wow fadeInLeft" data-wow-delay=".3s" style={ { visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInLeft' } }>
+                <img className="ss1" alt="#" data-pagespeed-url-hash="2622135838" src={ rtl } />
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-12 col-12">
+
+              <div
+                className="info-text wow fadeInRight"
+                data-wow-delay=".5s"
+                style={ { visibility: 'visible', animationDelay: '0.5s', animationName: 'fadeInRight' } }
+              >
+                <h2>Testing Library</h2>
+                <p>
+                  Projeto em
+                  {' '}
+                  <strong>React</strong>
+                  {' '}
+                  e
+                  {' '}
+                  <strong>TypeScript</strong>
+                  {' '}
+                  desenvolvendo habilidades de testes de integração com a biblioteca <strong>React Testing Library</strong>.
+                  {' '}
+                  <br />
+                  Você irá realizar testes de integração com a <strong>React Testing Library</strong> em uma aplicação que simula uma pokedex.
+                </p>
+                <div className="button">
+                  <a href="" className="btn">Iniciar</a>
                 </div>
               </div>
 
@@ -69,6 +142,7 @@ function Projects() {
           </div>
         </div>
       </div>
+      
     </section>
   );
 }
