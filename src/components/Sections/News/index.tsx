a48bf15a511168bdb0db6d135a638609fdfb34b5
Danilo Vieira <danilo_strvieira@hotmail.com>
2023-05-18 14:53:15 -0300

    refactor: adds course content and icons to cards in news section

--- src/components/Sections/News/index.tsx
+++ src/components/Sections/News/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Atom,
+  Code,
+  FileTsx,
+  Textbox,
+  Browser,
+  FileCss,
+} from '@phosphor-icons/react';
+
 function News() {
   return (
     <section id="news" className="features section">
@@ -5,80 +14,172 @@ function News() {
         <div className="row">
           <div className="col-12">
             <div className="section-title">
-              <h3 className="wow zoomIn" data-wow-delay=".2s">Novidades</h3>
+              <h3 className="wow zoomIn" data-wow-delay=".2s">
+                Novidades
+              </h3>
               <h2 className="wow fadeInUp" data-wow-delay=".4s">
                 O módulo de Front-end está melhorando a cada dia!
               </h2>
-              <p className="wow fadeInUp" data-wow-delay=".6s">A Turma 32 começa com muitas novidades no módulo, tecnologias novas, projetos novos e muita, mas muita prática.</p>
+              <p className="wow fadeInUp" data-wow-delay=".6s">
+                A Turma 32 começa com muitas novidades no módulo, tecnologias
+                novas, projetos novos e muita, mas muita prática.
+              </p>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".2s">
-              <i className="lni lni-cloud-upload" />
-              <h3>React</h3>
-              <p>A biblioteca front-end mais utilizada do mundo está aqui.</p>
+        <div
+          className="row"
+          style={ {
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gap: 16,
+          } }
+        >
+          <div style={ { padding: 0 } }>
+            <div
+              style={ { height: '100%' } }
+              className="single-feature wow fadeInUp"
+              data-wow-delay=".2s"
+            >
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <Atom style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>React</h3>
+              <p>A biblioteca front-end mais utilizada do mundo está aqui. Aprenderemos como podemos usá-la para construir aplicações</p>
             </div>
-
           </div>
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".4s">
-              <i className="lni lni-lock" />
-              <h3>React Hooks</h3>
-              <p>Hooks é a forma mais moderna de construir aplicações com React, e em front-end vamos aprofundar nesta funcionalidade desde a segunda sessão.</p>
-            </div>
-
-          </div>
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".6s">
-              <i className="lni lni-reload" />
-              <h3>Simple Queues</h3>
+          <div style={ { padding: 0 } }>
+            <div style={ { height: '100%' } } className="single-feature wow fadeInUp" data-wow-delay=".4s">
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <Code style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>React Hooks</h3>
               <p>
-                It is a long established fact that a reader will be distracted by the readable content of a
-                page at its layout.
+                Hooks é a forma mais moderna de construir aplicações com React,
+                e em front-end vamos aprofundar nesta funcionalidade desde a
+                segunda sessão.
               </p>
             </div>
-
           </div>
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".2s">
-              <i className="lni lni-shield" />
-              <h3>Advanced Security</h3>
+          <div style={ { padding: 0 } }>
+            <div style={ { height: '100%' } } className="single-feature wow fadeInUp" data-wow-delay=".6s">
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <FileTsx style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>TypeScript + Vite</h3>
               <p>
-                It is a long established fact that a reader will be distracted by the readable content of a
-                page at its layout.
+                Iremos trabalhar com TypeScript e Vite, duas tecnologias que estão em alta no mercado e que vão te ajudar
+                a construir aplicações mais robustas e performáticas.
               </p>
             </div>
-
           </div>
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".4s">
-              <i className="lni lni-cog" />
-              <h3>Powerful API</h3>
+          <div style={ { padding: 0 } }>
+            <div style={ { height: '100%' } } className="single-feature wow fadeInUp" data-wow-delay=".2s">
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <Textbox style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>Formulários</h3>
               <p>
-                It is a long established fact that a reader will be distracted by the readable content of a
-                page at its layout.
+                Iremos mergulhar na construção de formulários e validação de campos no React, adquirindo habilidades
+                essenciais para criar interações dinâmicas e seguras em nossas aplicações.
               </p>
             </div>
-
           </div>
-          <div className="col-lg-4 col-md-6 col-12">
-
-            <div className="single-feature wow fadeInUp" data-wow-delay=".6s">
-              <i className="lni lni-layers" />
-              <h3>Database Backups</h3>
+          <div style={ { padding: 0 } }>
+            <div style={ { height: '100%' } } className="single-feature wow fadeInUp" data-wow-delay=".4s">
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <Browser style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>React Router</h3>
               <p>
-                It is a long established fact that a reader will be distracted by the readable content of a
-                page at its layout.
+                No módulo React Router, aprenderemos a criar rotas dinâmicas
+                para nossa aplicação, permitindo a navegação entre diferentes componentes e páginas de forma simples e eficiente. Vamos construir uma experiência de usuário imersiva!
               </p>
             </div>
-
+          </div>
+          <div style={ { padding: 0 } }>
+            <div style={ { height: '100%' } } className="single-feature wow fadeInUp" data-wow-delay=".6s">
+              <div
+                style={ {
+                  width: 60,
+                  height: 60,
+                  backgroundColor: '#1DB700',
+                  borderRadius: '7px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#ffffff',
+                  boxShadow: '0px 4px 6px #0000002a',
+                } }
+              >
+                <FileCss style={ { width: 24, height: 24 } } />
+              </div>
+              <h3 style={ { marginTop: 30 } }>Estilização</h3>
+              <p>
+                Vamos aprender a aplicar estilos personalizados e explorar técnicas avançadas para deixar suas aplicações visualmente deslumbrantes e
+                consistentes. Vamos elevar a estilização das nossas aplicações React a um novo nível!
+              </p>
+            </div>
           </div>
         </div>
       </div>
