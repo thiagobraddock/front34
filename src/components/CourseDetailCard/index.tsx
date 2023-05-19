626aba0f0d8f1252cbde32b495b42b01828314c6
Arthur Procópio <avprocopio@gmail.com>
2023-05-19 13:27:29 -0300

    add new content tooltip

--- src/components/CourseDetailCard/index.tsx
+++ src/components/CourseDetailCard/index.tsx
@@ -1,13 +1,19 @@
+import { Star } from '@phosphor-icons/react';
+import Tooltip from '@mui/material/Tooltip';
 import { useState } from 'react';
 
 type CourseDetailCardProps = {
   title: string;
-  topics: string[];
+  topics: {
+    title: string;
+    isNew?: boolean;
+  }[];
   description: string;
   numberOfDays: number;
+  sessionNumber: number;
 };
 
-function CourseDetailCard({ title, topics, description, numberOfDays }: CourseDetailCardProps) {
+function CourseDetailCard({ title, topics, description, numberOfDays, sessionNumber }: CourseDetailCardProps) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const initialTopics = topics.slice(0, 3);
@@ -19,7 +25,7 @@ function CourseDetailCard({ title, topics, description, numberOfDays }: CourseDe
         className="single-table wow fadeInUp"
         data-wow-delay=".2s"
       >
-        <div className="table-head">
+        <div className="table-head" style={ { position: 'relative' } }>
           <h4 className="title">{ title }</h4>
           <p>{description}</p>
           <div className="price">
@@ -28,26 +34,60 @@ function CourseDetailCard({ title, topics, description, numberOfDays }: CourseDe
               <span className="duration"> Dias de conteúdo e projetos</span>
             </h2>
           </div>
+          <div className="container-background">{sessionNumber}</div>
         </div>
         <div className="table-content">
           <h4 className="middle-title">O que será visto:</h4>
-
           <ul className="table-list">
             {
               initialTopics.map((topic) => (
-                <li key={ topic }>
-                  <i className="lni lni-checkmark-circle" />
+                <li key={ topic.title }>
+                  {
+                    topic.isNew
+                      ? (
+                        <i style={ { display: 'flex', alignItems: 'center' } }>
+                          <Star size={ 15 } color="#FFE500" weight="fill" />
+                        </i>
+                      )
+                      : <i className="lni lni-checkmark-circle" />
+                  }
                   {' '}
-                  { topic }
+                  {
+                    topic.isNew ? (
+                      <Tooltip title="Novo" placement="top-end">
+                        <strong style={ { color: '#1DB700' } }>
+                          { topic.title }
+                        </strong>
+                      </Tooltip>
+                    )
+                      : topic.title
+                  }
                 </li>
               ))
             }
             {
               isExpanded && restOfTopics.map((topic) => (
-                <li key={ topic }>
-                  <i className="lni lni-checkmark-circle" />
+                <li key={ topic.title }>
+                  {
+                    topic.isNew
+                      ? (
+                        <i style={ { display: 'flex', justifyContent: 'center' } }>
+                          <Star size={ 15 } color="#FFE500" weight="fill" />
+                        </i>
+                      )
+                      : <i className="lni lni-checkmark-circle" />
+                  }
                   {' '}
-                  { topic }
+                  {
+                    topic.isNew ? (
+                      <Tooltip title="Novo" placement="top-end">
+                        <strong style={ { color: '#1DB700' } }>
+                          { topic.title }
+                        </strong>
+                      </Tooltip>
+                    )
+                      : topic.title
+                  }
                 </li>
               ))
             }
--- src/components/Sections/CourseDetails/index.tsx
+++ src/components/Sections/CourseDetails/index.tsx
@@ -18,102 +18,271 @@ function CourseDetails() {
         <CourseDetailCard
           title="Introdução ao Front-end e Javascript Assíncrono"
           topics={ [
-            'Criação de projetos com Vite',
-            'Promises',
-            'Async Await',
-            'Testes assíncronos',
-            'Novo projeto Ichoveu',
+            {
+              title: 'Criação de projetos com Vite',
+              isNew: false,
+            },
+            {
+              title: 'Promises',
+              isNew: false,
+            },
+            {
+              title: 'Async Await',
+              isNew: false,
+            },
+            {
+              title: 'Testes assíncronos',
+              isNew: false,
+            },
+            {
+              title: 'Novo projeto Ichoveu',
+              isNew: true,
+            },
           ] }
           description="Os primeiros passos em um novo mundo"
           numberOfDays={ 5 }
+          sessionNumber={ 1 }
         />
         <CourseDetailCard
           title="Introdução ao React"
           topics={ [
-            'Criação de projetos React + Typescript com Vite',
-            'Criação de componentes funcionais',
-            'Introdução ao TypeScript',
-            'Props',
-            'Tipagem de props com TypeScript',
-            'Renderização condicional',
-            'Renderização de listas',
-            'Estilização e utilização de imagens em componentes',
-            'Event Handlers',
-            'Utilizando estados com o hook useState',
-            'Criando estados com tipos genéricos',
-            'Formulários',
-            'Elevação de estado',
-            'Validando campos',
+            {
+              title: 'Criação de projetos React + Typescript com Vite',
+              isNew: true,
+            },
+            {
+              title: 'Criação de componentes funcionais',
+              isNew: false,
+            },
+            {
+              title: 'Introdução ao TypeScript',
+              isNew: false,
+            },
+            {
+              title: 'Props',
+              isNew: false,
+            },
+            {
+              title: 'Tipagem de props com TypeScript',
+              isNew: true,
+            },
+            {
+              title: 'Renderização condicional',
+              isNew: false,
+            },
+            {
+              title: 'Renderização de listas',
+              isNew: false,
+            },
+            {
+              title: 'Estilização e utilização de imagens em componentes',
+              isNew: false,
+            },
+            {
+              title: 'Event Handlers',
+              isNew: false,
+            },
+            {
+              title: 'Utilizando estados com o hook useState',
+              isNew: true,
+            },
+            {
+              title: 'Criando estados com tipos genéricos',
+              isNew: true,
+            },
+            {
+              title: 'Formulários',
+              isNew: false,
+            },
+            {
+              title: 'Elevação de estado',
+              isNew: false,
+            },
+            {
+              title: 'Validando campos',
+              isNew: false,
+            },
           ] }
           description="A base para o desenvolvimento front-end"
           numberOfDays={ 9 }
+          sessionNumber={ 2 }
         />
         <CourseDetailCard
           title="Avançando em React"
           topics={ [
-            'React Router (com componentes funcionais)',
-            'Rotas dinâmicas',
-            'Outlet',
-            'Hook useLocation',
-            'Hook useEffect',
+            {
+              title: 'React Router (com componentes funcionais)',
+              isNew: true,
+            },
+            {
+              title: 'Rotas dinâmicas',
+              isNew: false,
+            },
+            {
+              title: 'Outlet',
+              isNew: true,
+            },
+            {
+              title: 'Hook useLocation',
+              isNew: true,
+            },
+            {
+              title: 'Hook useEffect',
+              isNew: true,
+            },
           ] }
           description="Aprofundando no desenvolvimento front-end"
           numberOfDays={ 7 }
+          sessionNumber={ 3 }
         />
         <CourseDetailCard
           title="Testando aplicações React"
           topics={ [
-            'Configurando ambiente de teste',
-            'React Testing Library',
-            'Testando um componente',
-            'Testando eventos',
-            'Mocks',
-            'Testando rotas',
+            {
+              title: 'Configurando ambiente de teste',
+              isNew: false,
+            },
+            {
+              title: 'React Testing Library',
+              isNew: false,
+            },
+            {
+              title: 'Testando um componente',
+              isNew: false,
+            },
+            {
+              title: 'Testando eventos',
+              isNew: false,
+            },
+            {
+              title: 'Mocks',
+              isNew: false,
+            },
+            {
+              title: 'Testando rotas',
+              isNew: true,
+            },
           ] }
           description="Aprendendo a testar aplicações React"
           numberOfDays={ 5 }
+          sessionNumber={ 4 }
         />
         <CourseDetailCard
           title="Estilização e metodologias ágeis"
           topics={ [
-            'Estilização',
-            'Manifesto ágil',
-            'Kanban',
-            'Scrum',
+            {
+              title: 'Estilização',
+              isNew: true,
+            },
+            {
+              title: 'Manifesto ágil',
+              isNew: false,
+            },
+            {
+              title: 'Kanban',
+              isNew: false,
+            },
+            {
+              title: 'Scrum',
+              isNew: false,
+            },
           ] }
           description="Aprofundando na estilização e metodologias ágeis"
           numberOfDays={ 7 }
+          sessionNumber={ 5 }
         />
         <CourseDetailCard
           title="Gerenciamento de estados com Redux"
           topics={ [
-            'Peças do Redux',
-            'Redux Dev Tools',
-            'Redux no React',
-            'Lib react-redux (hooks useSelector e useDispatch)',
-            'Redux Thunk',
-            'Testes com Redux',
+            {
+              title: 'Peças do Redux',
+              isNew: false,
+            },
+            {
+              title: 'Redux Dev Tools',
+              isNew: false,
+            },
+            {
+              title: 'Redux no React',
+              isNew: false,
+            },
+            {
+              title: 'Lib react-redux (hooks useSelector e useDispatch)',
+              isNew: true,
+            },
+            {
+              title: 'Redux Thunk',
+              isNew: true,
+            },
+            {
+              title: 'Testes com Redux',
+              isNew: false,
+            },
           ] }
           description="A ferramenta de gerenciamento de estados mais utilizado no mercado"
           numberOfDays={ 9 }
+          sessionNumber={ 6 }
         />
         <CourseDetailCard
           title="Componentes de classe, ContextAPI e
           Hooks customizados"
           topics={ [
-            'Componentes de classes e ciclo de vida',
-            'ContextAPI',
-            'Criação de Hooks Customizados',
-            'Utiliação de Typescript',
+            {
+              title: 'Componentes de classes e ciclo de vida',
+              isNew: false,
+            },
+            {
+              title: 'ContextAPI',
+              isNew: false,
+            },
+            {
+              title: 'Criação de Hooks Customizados',
+              isNew: false,
+            },
+            {
+              title: 'Utilização de Typescript',
+              isNew: true,
+            },
           ] }
           description="Os toques finais para o desenvolvimento front-end"
           numberOfDays={ 8 }
+          sessionNumber={ 7 }
         />
         <CourseDetailCard
           title="Projeto final"
-          topics={ [] }
+          topics={ [
+            {
+              title: 'Utilizar Redux para gerenciar estado.',
+              isNew: false,
+            },
+            {
+              title: 'Utilizar a biblioteca React-Redux.',
+              isNew: false,
+            },
+            {
+              title: 'Utilizar a Context API do React para gerenciar estado.',
+              isNew: false,
+            },
+            {
+              title: 'Utilizar o React Hook useState.',
+              isNew: false,
+            },
+            {
+              title: 'Utilizar o React Hook useContext.',
+              isNew: false,
+            },
+            {
+              title: 'Utilizar o React Hook useEffect.',
+              isNew: false,
+            },
+            {
+              title: 'Criar Hooks customizados.',
+              isNew: false,
+            },
+          ] }
           description="O projeto final do curso"
           numberOfDays={ 11 }
+          sessionNumber={ 8 }
         />
       </div>
     </div>
