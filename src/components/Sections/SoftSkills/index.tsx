f393808bf24e63871d4a124a17655cd36ef03290
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2023-08-02 13:52:51 -0300

    refact: softskills e carreira

--- src/components/Sections/SoftSkills/index.tsx
+++ src/components/Sections/SoftSkills/index.tsx
@@ -1,13 +1,41 @@
+/* eslint-disable react/no-multi-comp */
 import {
-  Atom,
-  Code,
-  FileTsx,
-  Textbox,
-  Browser,
-  FileCss,
+  Lightbulb,
+  Brain,
+  UsersFour,
+  Handshake,
+  LinkedinLogo,
+  GithubLogo,
+  GitBranch,
+  PresentationChart,
 } from '@phosphor-icons/react';
 
 import './index.css';
+
+const skillData = [
+  { icon: Lightbulb, title: 'Criatividade e Inovação', delay: '.2s', description: 'Como utilizar a criatividade na resolução de problemas' },
+  { icon: Brain, title: 'Brainstorm', delay: '.4s', description: 'Diferentes técnicas para gerar ideias' },
+  { icon: UsersFour, title: 'Colaboração', delay: '.6s', description: 'A importância colaboração em times de desenvolvimento, e como desenvolvê-la' },
+  { icon: Handshake, title: 'Criatividade + Colaboração', delay: '.2s', description: 'Como utilizar a colaboração para aumentar a criatividade' },
+  { icon: LinkedinLogo, title: 'Linkedin', delay: '.4s', description: 'Como construir um perfil estratégico' },
+  { icon: GithubLogo, title: 'Github', delay: '.6s', description: 'Desenvolvendo um GitHub campeão' },
+  { icon: GitBranch, title: 'Projetos em grupo', delay: '.6s', description: 'Aprendendo a colaborar utilizando metodologias ágeis, e o passo a passo para apresentar o seu projeto' },
+  { icon: PresentationChart, title: 'Apresentação de Projetos', delay: '.6s', description: 'Como apresentar projetos de forma eficaz' },
+];
+
+function SkillCard({ icon: Icon, title, delay, description }) {
+  return (
+    <div className="col-lg-4 col-md-6 col-12">
+      <div className="single-feature wow fadeInUp" data-wow-delay={ delay }>
+        <div className="iconBg">
+          <Icon />
+        </div>
+        <h3 className="mt-3">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 
 function SoftSkills() {
   return (
@@ -15,7 +43,7 @@ function SoftSkills() {
       <div className="row">
         <div className="col-12">
           <div className="section-title" style={ { width: '100%' } }>
-            <h3 className="wow zoomIn" data-wow-delay=".2s">
+            <h3 className="wow zoomIn" data-wow-delay=".2s" style={ { color: '#7e18de' } }>
               SoftSkills e Carreira
             </h3>
 
@@ -25,92 +53,11 @@ function SoftSkills() {
           </div>
         </div>
       </div>
-      <div
-        className="row"
-      >
-        <div className="col-lg-4 col-md-6 col-12">
-          <div
-            className="single-feature wow fadeInUp"
-            data-wow-delay=".2s"
-          >
-            <div
-              className="iconBg"
-            >
-              <Atom />
-            </div>
-            <h3 className="mt-3">React</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="single-feature wow fadeInUp" data-wow-delay=".4s">
-            <div
-              className="iconBg"
-            >
-              <Code />
-            </div>
-            <h3 className="mt-3">React Hooks</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="single-feature wow fadeInUp" data-wow-delay=".6s">
-            <div
-              className="iconBg"
-            >
-              <FileTsx />
-            </div>
-            <h3 className="mt-3">TypeScript + Vite</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="single-feature wow fadeInUp" data-wow-delay=".2s">
-            <div
-              className="iconBg"
-            >
-              <Textbox />
-            </div>
-            <h3 className="mt-3">Formulários</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="single-feature wow fadeInUp" data-wow-delay=".4s">
-            <div
-              className="iconBg"
-            >
-              <Browser />
-            </div>
-            <h3 className="mt-3">React Router</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 col-12">
-          <div className="single-feature wow fadeInUp" data-wow-delay=".6s">
-            <div
-              className="iconBg"
-            >
-              <FileCss />
-            </div>
-            <h3 className="mt-3">Estilização</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod dolorem alias aliquid tenetur.
-            </p>
-          </div>
-        </div>
+      <div className="row">
+        {skillData.map((skill, index) => (
+          <SkillCard key={ index } { ...skill } />
+        ))}
       </div>
-
     </div>
   );
 }
